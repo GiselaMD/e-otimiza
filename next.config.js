6c1c0883d47797875ab69a9a871f861896ec7b1e
Gisela Miranda Difini <gisela.mdi@gmail.com>
2021-11-20 14:59:14 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    API_URL: 'https://epdv-api.herokuapp.com',
+    API_URL: 'https://epdv-api.herokuapp.com/',
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       'pk_test_51IwoaUI3DLjCBNrBu4JMlNkoYYi7ueqXRxLnKHdsp8F25h0zAjVcHmt47CxBRwaAVcYHrpqQkmxstXLDw1lQsIaR00tqOzKtUK',
   },
